072cef98f62160490ed47c0034ba65f2ae8a039d
Artemii_Khokhlov <artemii.hhlov@gmail.com>
2025-03-24 21:06:31 +0600

    first commit

---
+++ src/main/java/com/khokhlov/tendermonitoring/TenderMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.khokhlov.tendermonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenderMonitoringApplication.class, args);
+    }
+
+}
