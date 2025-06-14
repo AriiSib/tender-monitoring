1c40ed490ef66c7376e9db59fa2d1a28d6b2dc0f
Artemii_Khokhlov <artemii.hhlov@gmail.com>
2025-06-14 23:24:40 +0600

    refactor: remove useless tender table from liquibase

--- src/main/java/com/khokhlov/tendermonitoring/repository/TenderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.khokhlov.tendermonitoring.repository;
-
-import com.khokhlov.tendermonitoring.model.entity.Tender;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TenderRepository extends JpaRepository<Tender, Long> {
-}
